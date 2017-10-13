5fac40b990b7bfcbe30769678a524def561db310
unstream <unstream@web.de>
2017-10-13 08:27:45 +0200

    added bootstrap

---
+++ src/app/fractal/fractal.ts
@@ -0,0 +1,11 @@
+export class Fractal {
+
+  constructor(
+    public c0: number,
+    public c0i: number,
+    public c1: number,
+    public c1i: number,
+    public maxIterations: number
+  ) {  }
+
+}
