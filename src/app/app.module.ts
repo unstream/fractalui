0e0917e6ef8d5b21b4b5ae9e2d5e2eb346b101f9
unstream <unstream@web.de>
2017-10-13 08:19:01 +0200

    Added bootstrap support.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { FractalComponent } from './fractal/fractal.component';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    FractalComponent
+    FractalComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule, HttpClientModule, FormsModule
--- src/app/fractal/fractal.component.ts
+++ src/app/fractal/fractal.component.ts
@@ -1,5 +1,8 @@
 import {AfterViewInit, Component, OnInit, ViewChild} from "@angular/core";
 import {HttpClient} from '@angular/common/http';
+import { HttpParams } from '@angular/common/http';
+import {NgForm} from '@angular/forms';
+import {Fractal} from './fractal';
 
 //import {HostListener} from "@angular/core";
 
@@ -11,12 +14,16 @@ import {HttpClient} from '@angular/common/http';
 
 export class FractalComponent implements AfterViewInit, OnInit {
     static readonly SQRT075 = Math.sqrt(0.75);
-    iterations = 8;
-  maxIterations = 9;
+
+  iterations = 8;
+  maxIterations = 100;
 
 
   color = "#ff6b09";
   color2 = "#ffad20";
+
+  model = new Fractal(-1.5, -1, 0.5, 1, 100);
+
   context: CanvasRenderingContext2D;
 
   @ViewChild("myCanvas") myCanvas;
@@ -60,11 +67,24 @@ export class FractalComponent implements AfterViewInit, OnInit {
   }
 
   ngOnInit(): void {
-      // Make the HTTP request:
-      this.http.get<LongQuad>('http://192.168.99.100:8080/fractalservice/api/fractal/longquad?iterations=100').subscribe(data => {
+      let params = new HttpParams()
+        .set("c0", '' + this.model.c0)
+        .set("c0i", '' + this.model.c0i)
+        .set("c1", '' + this.model.c1)
+        .set("c1i", '' + this.model.c1i);
+      //  .set("iterations", '' + this.model.maxIterations);
+      this.http.get<LongQuad>('http://192.168.99.100:80/fractalservice/api/fractal/longquad', {params}).subscribe(data => {
       this.data = data.data;
+      console.log("draw");
       this.draw();
     });
+  }
+
+  onSubmit(f: NgForm) {
+    console.log(f.form.value);
+    this.model = f.form.value;
+    console.log("c0",this.model.c0);
+    this.ngOnInit();
   }
 
   draw() {
@@ -77,7 +97,11 @@ export class FractalComponent implements AfterViewInit, OnInit {
         if (this.data[x][y] > this.maxIterations) {
           this.maxIterations = this.data[x][y];
         }
-        if (this.data[x][y] > this.iterations) {
+        if (this.data[x][y] == this.maxIterations) {
+          ctx.fillStyle = "#00ff00";
+          ctx.fillRect( x, y, 1, 1 );
+        } else {
+          ctx.fillStyle = this.interpolateColor(this.color2, this.color, (1.0 * this.data[x][y])/this.maxIterations);
           ctx.fillRect( x, y, 1, 1 );
         }
       }
